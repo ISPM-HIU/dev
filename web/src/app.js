24603b89514a98e6126c83aa164fc6645277a3ee
Tsitoniaina <rasoamahazomanana01@gmail.com>
2024-02-23 10:15:26 +0300

    add login

--- web/src/app.js
+++ web/src/app.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import AdminLayout from "layouts/Admin.js";
 import { UserContext } from "context/user";
-
+import Login from "views/login";
 export const App = ()=>{
     const {userId} = useContext(UserContext);
     const [path, setPath] = useState('');
@@ -14,6 +14,7 @@ export const App = ()=>{
         <BrowserRouter>
             <Switch>
                 <Route path="/admin" render={(props)=> <AdminLayout {...props} />}/>
+                <Route path="/login" render={(props) => <Login/>}/>
                 <Redirect from="/" to={path} />
             </Switch>
         </BrowserRouter>
